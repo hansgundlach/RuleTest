46d8cae2e349aae5d29b3926e592b3f5b0e8bdb6
Hans Gundlach <hans.gundlach@gmail.com>
2023-12-01 05:21:06 -0500

    removed redundency

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # A Study of Faithfulness in LLM Classification 
 
-This is code provided to test the faithfulnes of models in a classification task. FrozenExamples contains the the frozen examples classifications used in-context for some of our measures. Examples were also generated programmaticly using gen_flag_examples, and gen_code_example methods. rulepipe.py runs most of the experimenets used in our paper. 
+This is code provided to test the faithfulnes of models in a classification task.  Examples were generated programmaticly using gen_flag_examples, and gen_anagram_example methods in rulepipe.py. rulepipe.py runs most of the experimenets used in our paper. FrozenExamples contains randomly selected classification examples that we think are illustrative. 
+
+
--- utils.py
+++ utils.py
@@ -44,18 +44,6 @@ def extract_answer(text):
     return extracted_string
 
 
-# Function to append to conversation history and make API calls
-def ask_openai_full(question):
-    global conversation_history
-    full_prompt = f"{conversation_history}{question}\n"
-    response = openai.Completion.create(
-        engine="text-davinci-003", prompt=full_prompt, max_tokens=150
-    )
-    answer = response.choices[0].text.strip()
-    conversation_history += f"Q: {question}\nA: {answer}\n"
-    return answer
-
-
 def ask_openai(
     question,
     temperature=0.0,
@@ -74,30 +62,3 @@ def ask_openai(
         ],
     )
     return response.choices[0].message.content
-
-
-def random_string_from_file(file_path, length=30):
-    try:
-        with open(file_path, "r", encoding="utf-8") as file:
-            content = file.read()
-
-        if len(content) < length:
-            return "The file's content is shorter than the requested length."
-
-        start_index = random.randint(0, len(content) - length)
-        return content[start_index : start_index + length]
-    except FileNotFoundError:
-        return "File not found."
-    except Exception as e:
-        return str(e)
-
-
-# # Example usage
-# file_path = "/path/to/your/file.txt"  # Replace with the actual file path
-# random_string = random_string_from_file(file_path)
-# random_string
-
-
-# %%
-
-# look at performance of task as the number of examples increases.
